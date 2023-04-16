ebcd0f6ca1ef86d89456766b1dbc4a1405ed35cd
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-04-16 20:42:46 +0100

    test initial enter

--- src/components/key-value-add-field/key-value-add-field.spec.tsx
+++ src/components/key-value-add-field/key-value-add-field.spec.tsx
@@ -1,6 +1,17 @@
 import { render } from "ink-testing-library";
-import { KeyValueAddField } from "./key-value-add-field.js";
 import { jest } from "@jest/globals";
+import delay from "delay";
+
+jest.unstable_mockModule("uuid", () => ({
+  v4: jest.fn(),
+}));
+
+const { KeyValueAddField } = await import("./key-value-add-field.js");
+const { v4 } = await import("uuid");
+
+beforeEach(() => {
+  jest.mocked(v4).mockReset();
+});
 
 describe("<KeyValueAddField />", () => {
   it("Displays the correct label and a message requesting the user to 'press enter to add headers' if there is no data", () => {
@@ -16,5 +27,24 @@ describe("<KeyValueAddField />", () => {
 └──────────────────────────────────────────────────────────┘
 `.trim()
     );
+  });
+
+  it("Calls the onChange with an empty header if you press enter", async () => {
+    const onChange = jest.fn();
+
+    jest.mocked(v4).mockReturnValue("mock-id");
+
+    const { stdin } = render(
+      <KeyValueAddField label="Headers" onChange={onChange} />
+    );
+
+    stdin.write(`\r`);
+
+    expect(onChange).toBeCalledWith({
+      "mock-id": {
+        key: "",
+        value: "",
+      },
+    });
   });
 });
