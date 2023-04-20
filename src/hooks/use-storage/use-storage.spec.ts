847c0540930cd33701a6961242ea52a72e2fad92
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-04-20 20:37:49 +0100

    use storage is fully tested

--- src/hooks/use-storage/use-storage.spec.ts
+++ src/hooks/use-storage/use-storage.spec.ts
@@ -1,9 +1,10 @@
 import { mkdtemp, rmdir } from "fs/promises";
-import { renderHook, waitFor } from "@testing-library/react";
+import { renderHook, waitFor, act } from "@testing-library/react";
 import { useStorage } from "./use-storage.js";
 import { join } from "node:path";
 import fs from "fs/promises";
 import os from "os";
+import { HttpRequest } from "../../types/http-request.js";
 
 let path: string | undefined;
 
@@ -33,6 +34,62 @@ describe("useStorage", () => {
         expect(JSON.parse(result)).toStrictEqual([]);
       }
     });
+  });
+
+  it("Calling the update method results in the file being updated and the updated data being returned from the hook", async () => {
+    const data = `[
+  {
+    "id": "39f45798-94fd-4a79-b9d5-164aaeddb412",
+    "title": "Check Google",
+    "slug": "google",
+    "method": "GET",
+    "host": "http://www.google.com",
+    "path": "foo/bar",
+    "headers": {
+      "0fd6256b-da98-4e46-bc5e-36785528ed7a": {
+        "key": "content-type",
+        "value": "application/json"
+      }
+    }
+  },
+  {
+    "id": "cf512f76-74da-446b-8764-1f6ba09cb356",
+    "title": "Random dog facts",
+    "slug": "dog-facts",
+    "method": "GET",
+    "host": "https://dog-facts-api.herokuapp.com",
+    "path": "api/v1/resources/dogs/all"
+  }
+]`;
+
+    await fs.writeFile(`${path}/requests.json`, data);
+
+    const { result } = renderHook(() =>
+      useStorage<HttpRequest[]>(`${path}/requests.json`)
+    );
+
+    await waitFor(() => {
+      expect(result.current.loaded).toBeTruthy();
+    });
+
+    if (result.current.loaded) {
+      const dataParsed = JSON.parse(data) as HttpRequest[];
+      dataParsed[1].title = "Random cat facts";
+      await act(async () => {
+        if (result.current.loaded) {
+          await result.current.update(dataParsed);
+        }
+      });
+
+      await waitFor(async () => {
+        expect(result.current.loaded).toBeTruthy();
+        if (result.current.loaded) {
+          expect(result.current.content).toStrictEqual(dataParsed);
+        }
+        const fileContents = await fs.readFile(`${path}/requests.json`, `utf8`);
+        expect(JSON.parse(fileContents)).toStrictEqual(dataParsed);
+      });
+    }
   });
 
   it("Loads data from disk", async () => {
