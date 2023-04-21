fcb686327beda6cf3f55ee0ba6faf898e68924a4
Ben Wainwright <ben.wainwright@cinch.co.uk>
2023-04-21 14:38:49 +0100

    fix: fixed type error

--- src/core/load-data.ts
+++ src/core/load-data.ts
@@ -5,7 +5,7 @@ import { HttpRequest } from "../types/http-request.js";
 export const loadData = async (path?: string): Promise<HttpRequest[]> => {
   try {
     return JSON.parse(
-      await fs.readFile(path ?? defaultDatafile, "utf8")
+      await fs.readFile(path ?? defaultDatafile(), "utf8")
     ) as HttpRequest[];
   } catch {
     return [];
